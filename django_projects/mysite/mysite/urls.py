09b9d7f605b8f1751ed1aa50ae772a510474b4da
Amy Dang <adang901@gmail.com>
2022-01-22 13:44:27 -0500

    Add starfish app

--- django_projects/mysite/mysite/urls.py
+++ django_projects/mysite/mysite/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('starfish/', include('starfish.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ django_projects/mysite/starfish/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StarfishConfig(AppConfig):
+    name = 'starfish'
