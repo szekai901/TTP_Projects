ae4a955250bb2aa25f087ee16a916de3389d3167
Amy Dang <adang901@gmail.com>
2022-01-23 20:36:40 -0500

    Add dad jokes

---
+++ frontend_projects/dad_jokes/script.js
@@ -0,0 +1,24 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+// USING ASYNC/AWAIT
+
+// Asynchonous function: it will run two function (or two things) simultaneously
+
+async function generateJoke(){
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    }
+
+    const res = await fetch('https://icanhazdadjoke.com', config)
+
+    const data = await res.json()
+
+    jokeEl.innerHTML = data.joke
+}
